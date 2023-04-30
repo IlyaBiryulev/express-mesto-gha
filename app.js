11361139a110a50b19c3717f79ca76a0bbd215db
Ilya Biryulev <birulevila@yandex.ru>
2023-04-30 20:41:31 +0300

    сделать так, чтобы api не возвращал хеш пароля

--- app.js
+++ app.js
@@ -11,14 +11,6 @@ mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 
 app.use(express.json());
 
-app.use((req, res, next) => {
-  req.user = {
-    _id: '6442b89f2264795b94d51172',
-  };
-
-  next();
-});
-
 app.use(rootRouter);
 
 app.listen(PORT);
--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -26,7 +27,27 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
     minlength: 8,
+    select: false,
   },
-}, { versionKey: false });
+}, {
+  versionKey: false,
+  statics: {
+    findUserByCredentials(email, password) {
+      return this.findOne({ email }).select('+password')
+        .then((user) => {
+          if (!user) {
+            return Promise.reject(new Error('Неправильные почта или пароль'));
+          }
+          return bcrypt.compare(password, user.password)
+            .then((matched) => {
+              if (!matched) {
+                return Promise.reject(new Error('Неправильные почта или пароль'));
+              }
+              return user;
+            });
+        });
+    },
+  },
+});
 
 module.exports = mongoose.model('user', userSchema);
