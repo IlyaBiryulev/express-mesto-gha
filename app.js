55bc0d84b968ca91babfea57b878db5dbd32f190
Ilya Biryulev <birulevila@yandex.ru>
2023-04-30 21:13:30 +0300

    поиск бага с роутами

--- app.js
+++ app.js
@@ -6,9 +6,10 @@ const { PORT = 3000 } = process.env;
 const auth = require('./middlewares/auth');
 const userRouter = require('./routes/users');
 const cardRouter = require('./routes/cards');
-const loginRouter = require('./routes/signin');
-const authRouter = require('./routes/signup');
+/* const loginRouter = require('./routes/signin');
+const authRouter = require('./routes/signup'); */
 const notFoundRouter = require('./routes/notFound');
+const { login, createUser } = require('./controllers/users');
 
 const app = express();
 
@@ -16,10 +17,13 @@ mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 
 app.use(express.json());
 
+app.post('/signin', login);
+app.post('/signup', createUser);
+
 app.use('/users', auth, userRouter);
 app.use('/cards', auth, cardRouter);
-app.use('/signin', loginRouter);
-app.use('/signup', authRouter);
+/* app.use('/signin', loginRouter);
+app.use('/signup', authRouter); */
 app.use(notFoundRouter);
 
 app.listen(PORT);
--- middlewares/auth.js
+++ middlewares/auth.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken');
 const { NODE_ENV, SECRET_KEY } = process.env;
 
 module.exports = (req, res, next) => {
-  const token = jwt.cookies.jwt;
+  const token = req.cookies.jwt;
   if (!token) {
     return next(
       res
