cce28fc195b6cfd5a84d0ac18fec33f1cf06d341
Ilya Biryulev <birulevila@yandex.ru>
2023-05-03 17:39:46 +0300

    изменить мидлвер ошибки

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,8 +1,8 @@
 const errorHandler = (err, req, res, next) => {
-  if (!err.statusCode) {
-    res.status(500).send({ message: err.message });
-  }
-  res.status(err.statusCode).send({ message: err.message });
+  const statusCode = err.statusCode || 500;
+
+  const message = statusCode === 500 ? 'На сервере произошла ошибка' : err.message;
+  res.status(statusCode).send({ message });
   next();
 };
 
